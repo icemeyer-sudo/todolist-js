64da49d352ed6872432b270a339c7ca5646c75f1
icemeyer-sudo <ice.meyer@gmail.com>
2026-05-01 22:54:22 +0200

    update

---
+++ README.md
@@ -0,0 +1,30 @@
+# Faire un todo-list en Javascript
+
+Projet de formation à Javascript : être capable de faire une todo-list complète, avec sauvegarde dans un fichier au format JSON.
+Le projet devra être réalisé sans IA.
+Idéalement il faudra à terme être capable de réaliser le projet en 1h, sans aide extérieur (IA, documentation, cheat sheet).
+Dans un second temps il faudra refaire le projet avec REACT, VUE et SOLIDJS pour pouvoir comparer les approches.
+
+## Fonctionnalités
+
+- Bouton pour ajouter une tâche
+- Les tâches s'affichent dans un liste à puce
+- Bouton reset pour effacer toute la liste (seulement si une liste existe)
+- Les tâches peuvent être supprimées
+- Les tâches doivent pouvoir être cochées ou décochées
+- Les tâches doivent être stockées dans un tableau d'objets (id, valeur, check, date)
+- On doit pouvoir effectuer un tri dans les tâches (par ordre alphabétique ou par date)
+- Toutes les actions doivent être sauvegardées dans un fichier JSON
+
+## Attention
+
+- On ne doit pas pouvoir ajouter une tâche vide
+- Si la liste est vide <ul> ne soit pas apparaitre dans le DOM
+- L'input doit être vidé à chaque ajout
+
+## Pour aller plus loin
+
+- Il doit y avoir un délai de 1s à chaque action avec un loader
+- Pendant cette seconde, tous les boutons sont désactivés
+- Le délai doit tenir compte du temps qu'il faut pour afficher / sauvegarder les données
+- S'il faut 200ms pour sauvegarder une donnée et l'afficher, il faut que le temps de chargement fasse 200ms + 800 ms volontaire = 1sec
--- main.js
+++ main.js
@@ -1,35 +1,104 @@
-const buttonTask = document.querySelector('#button');
+/* // Permet de connaitre le nombre d'eventListener en cours -- Code à mettre dans la console
+
+    Array.from(document.querySelectorAll('*'))
+        .reduce(function(pre, dom){
+        var clks = getEventListeners(dom).click;
+        pre += clks ? clks.length || 0 : 0;
+        return pre
+    }, 0)
+
+*/
+
+const buttonAddTask = document.querySelector('#button');
 const newTask = document.querySelector('#input-task');
 const body = document.body;
+let id = 0;
 
-buttonTask.addEventListener('click', handleClick);
-
-function handleClick(e) {
+buttonAddTask.addEventListener('click', handleClickAdd);
+function handleClickAdd(e) {
     console.log('Ajout d\'une nouvelle tâche');
     e.preventDefault();
-    addTask(newTask.value);
+    addTask(newTask);
 }
 
 function addTask(newTask) {
-    if (!newTask) { return }
+    if (!newTask.value) { return }
     createList();
     const ul = document.querySelector('#ul-task');
     const li = document.createElement('li');
+    li.setAttribute('data-id', id)
     const checkbox = document.createElement('button');
     checkbox.setAttribute('class', 'checkbox-task');
     const deleteButton = document.createElement('button');
     deleteButton.setAttribute('class', 'button-delete');
+    deleteButton.setAttribute('data-id', id);
     deleteButton.textContent = 'Supprimer la tâche';
-    li.textContent = newTask;
+    li.textContent = newTask.value;
     ul.append(li);
     li.prepend(checkbox);
     li.append(deleteButton);
+    id++;
+    newTask.value = '';
+}
+
+function handleClickDelete(e) {
+    const ul = document.querySelector('#ul-task');
+    deleteTask(e);
+    if (!ul.querySelector('li')) {
+        const btnDeleteAll = document.querySelector('#btn-delete-all');
+        ul.remove();
+        btnDeleteAll.remove();
+    }
+}
+
+function deleteTask(e) {
+    const taskId = e.target.getAttribute('data-id');
+    const ul = document.querySelector('#ul-task');
+    const li = ul.querySelector(`li[data-id='${taskId}']`);
+    li.remove();
 }
 
 function createList() {
     if (!document.querySelector('#ul-task')) {
-        const ul = document.createElement('ul');
-        ul.setAttribute('id', 'ul-task');
-        body.appendChild(ul);
+        createUl();
+        createEventListener();
+        createBtnDeleteAll();
     }
+}
+
+function createUl() {
+    const ul = document.createElement('ul');
+    ul.setAttribute('id', 'ul-task');
+    body.appendChild(ul);
+}
+
+function createEventListener() {
+    const ul = document.querySelector('#ul-task');
+    ul.addEventListener('click', (e) => {
+        if(e.target.classList.contains("button-delete")) {
+            handleClickDelete(e);
+        }
+    });
+}
+
+function createBtnDeleteAll() {
+    const btnAdd = document.querySelector('#button');
+    const btnDeleteAll = document.createElement('button');
+    btnDeleteAll.addEventListener('click', (e) => {
+        handleDeleteAll(e);
+    })
+    btnDeleteAll.setAttribute('id', 'btn-delete-all');
+    btnDeleteAll.textContent = 'Supprimer toutes les tâches';
+    btnAdd.after(btnDeleteAll);
+}
+
+function handleDeleteAll() {
+    const ul = document.querySelector('#ul-task');
+    const btnDeleteAll = document.querySelector('#btn-delete-all');
+    // Supprimer les eventListeners ici n'est pas obligatoire
+    // La suppression du DOM netoie automatiquement les eventListeners
+    ul.removeEventListener('click', handleClickDelete);
+    btnDeleteAll.removeEventListener('click', handleDeleteAll);
+    ul.remove();
+    btnDeleteAll.remove();
 }
